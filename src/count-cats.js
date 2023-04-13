286a7c0bfd92807fcd05995be3d89fec1ae8af91
Natalie Dub <111530283+favoritewillow@users.noreply.github.com>
2023-04-14 00:52:50 +0300

    feat: add count-cats.js (Friday 14.04.2023 0:52pm)

--- src/count-cats.js
+++ src/count-cats.js
@@ -1,9 +1,9 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const { NotImplementedError } = require("../extensions/index.js");
 
 /**
  * Given matrix where you have to find cats by ears "^^"
  *
- * @param {Array<Array>} matrix 
+ * @param {Array<Array>} matrix
  * @return {Number} count of cats found
  *
  * @example
@@ -14,11 +14,49 @@ const { NotImplementedError } = require('../extensions/index.js');
  * ]) => 3`
  *
  */
-function countCats(/* matrix */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+
+function logEachMatrixItem(matrix) {
+  console.log("\nLog each matrix item:");
+
+  for (let i = 0; i < matrix.length; i++) {
+    for (let j = 0; j < matrix[i].length; j++) {
+      console.log(`  item {${i}, ${j}} - ${matrix[i][j]}`);
+    }
+  }
 }
 
+function showMatrix(matrix) {
+  console.log("\nShow matrix:");
+
+  for (let i = 0; i < matrix.length; i++) {
+    console.log(matrix[i].join(" "));
+  }
+}
+
+function countCats(matrix) {
+  let countCatsYears = [];
+
+  let columnCount = matrix[0].length;
+
+  for (let i = 0; i < columnCount; i++) {
+    for (let j = 0; j < matrix.length; j++) {
+      if (matrix[j][i] === "^^") {
+        countCatsYears.push(i);
+      }
+    }
+  }
+
+  console.log(countCatsYears);
+}
+
+let matrix = [
+  [0, 1, "^^"],
+  [0, "^^", 2],
+  ["^^", 1, 2],
+];
+showMatrix(matrix);
+logEachMatrixItem(matrix);
+countCats(matrix);
 module.exports = {
-  countCats
+  countCats,
 };
