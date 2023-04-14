962f2e4b6918afb41f1cf364c3c2b83568f55a95
Natalie Dub <111530283+favoritewillow@users.noreply.github.com>
2023-04-14 14:27:23 +0300

    (Feat: add delete-digit.js (Friday 14.04.2023 14:26am))

--- src/delete-digit.js
+++ src/delete-digit.js
@@ -1,4 +1,4 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const { NotImplementedError } = require("../extensions/index.js");
 
 /**
  * Given some integer, find the maximal number you can obtain
@@ -11,11 +11,43 @@ const { NotImplementedError } = require('../extensions/index.js');
  * For n = 152, the output should be 52
  *
  */
-function deleteDigit(/* n */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function deleteDigit(number) {
+  let str = number.toString();
+  let arr = Array.from(str, Number);
+
+  let digit = [];
+
+  for (let i = 0; i < arr.length; i++) {
+    digit = arr[i];
+    console.log(digit);
+  }
+
+  function minNum(arr) {
+    let minNumber = Math.min(...arr);
+    let filteredNumbers = arr.filter((number) => number !== minNumber);
+    console.log(filteredNumbers);
+    let num = parseInt(filteredNumbers.join(""));
+    filteredNumbers.reverse();
+    let numReverse = parseInt(filteredNumbers.join(""));
+    console.log(num);
+    console.log(numReverse);
+
+    if (num > numReverse) {
+      return num;
+      //console.log(num);
+    } else if (num < numReverse) {
+      return numReverse;
+      //console.log(numReverse);
+    }
+    return -1;
+  }
+
+  minNum(arr);
 }
 
+let number = 152;
+deleteDigit(number);
+
 module.exports = {
-  deleteDigit
+  deleteDigit,
 };
